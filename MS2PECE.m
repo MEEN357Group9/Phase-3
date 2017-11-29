45574b4c7ee3c47d6584fca81949c4dba489039a
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-11-29 16:01:38 -0600

    should fix

--- MS2PECE.m
+++ MS2PECE.m
@@ -1,15 +1,15 @@
-function [T, X, V, A] = MS2PECE(X0, V0, A0, M, C, K FN, D)
+function [T, X, V, A] = MS2PECE(X0, V0, A0, M, C, K, FN, D)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-t = 0.5;
+
 siz = size(X0);
 T = zeros(1);
 X = zeros(1,siz(1));
 V = zeros(1,siz(1));
 A = zeros(1,siz(1));
-X(1) = X0;
-V(1) = V0;
-A(1) = A0;
+X(1,:) = X0;
+V(1,:) = V0;
+A(1,:) = A0;
 T(1) = D.t_in;
 
 %% Predict
@@ -18,34 +18,49 @@ T(1) = D.t_in;
 % step size
 h = (D.t_out - D.t_in)./D.N;
 
-X1p = X0 + h*V0 + h.^2./2*A0;
+X1p = X0 + h.*V0 + h.^2./2.*A0;
 
-V1p = V0 +h*A0;
+V1p = V0 +h.*A0;
 
 %% Evaluate
-A1p = M.^(-1) * ( FN(t,D) - C*V1p - K*X1p);
+A1p = M.^(-1) * ( FN(D) - C.*V1p - K.*X1p);
 
 %% Correct 
 
 t1 = D.t_in + h;
-X1 = X0 + h./2*(V1p + V0) - h.^2./12*(A1p-A0);
+X1 = X0 + h./2.*(V1p + V0) - h.^2./12.*(A1p-A0);
 
-V1 = V0 + h./2*(A1p+A0);
+V1 = V0 + h./2.*(A1p+A0);
 
 %% Evaluate
 
-A1 = M.^(-1) * (FN(t,D) - C*V1 - K*X1);
+A1 = M.^(-1) * (FN(D) - C.*V1 - K.*X1);
 
-T(2) = t1;
-X(2) = X1;
-V(2) = V1;
-A(2) = A1;
+T(2,:) = t1;
+X(2,:) = X1;
+V(2,:) = V1;
+A(2,:) = A1;
 
 %% Remaining
 for i = 2:D.N
     
     % predict
-    X1p = 1./3*(4 * 
+    X2p = 1./3.*(4.* X(i) - X(i-1))+ h./6.*(3.*V(i) + V(i-1))...
+        + h.^2./36.*(31.*A(i)-A(i-1)); 
+    V2p = 1./3.*(4.* V(i) - V(i-1)) + 2.*h./3.*(2.*A(i) - A(i-1));
+    
+    % evaluate
+    A2p = M.^(-1) * ( FN(D) - C.*V2p - K.*X2p);
+    T(i+1) = T(i) + h;
+    
+    X2 = 1./3.*(4.*X(i) - X(i-1)) + h./24.*(V2p + 14.*V(i) + V(i-1)) +...
+        h.^2./72.*(10.*A2p + 51.*A(i) - A(i-1));
+    V2 = 1./3.*(4*V(i) - V(i-1)) + 2.*h./3.*A2p;
+    
+    A2 = M.^(-1) * ( FN(D) - C.*V2 - K.*X2);
+    X(i+1,:) = X2;
+    V(i+1,:) = V2;
+    A(i+1,:) = A2;
     
 
 end
