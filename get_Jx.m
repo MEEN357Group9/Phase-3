ef94a6dcddfaaa76b547c8f5b93c75898ebc012e
HAshby <harleyashby@comcast.net>
2017-11-30 08:37:03 -0600

    sfrgt

--- get_Jx.m
+++ get_Jx.m
@@ -10,6 +10,7 @@ function [Jx] = get_Jx(FSAE_Race_Car)
 r_chassie=FSAE_Race_Car.chassis.diameter/24; %Finding the radius of the chassis in feet.
 m_chassis=FSAE_Race_Car.chassis.weight/32.174; %Mass of the chassis in slugs.
 %l_chassis=FSAE_Race_Car.chassis.length/12; %Length of the chassis in ft.
+%(not needed)
 d_chassis=FSAE_Race_Car.chassis.cg_Z/12; %Distance to use in the parallel axis theorm in ft.
 i_chassis_self=m_chassis*r_chassie^2; %Moment of inertia about its own center of gravity.
 i_chassis_cg=i_chassis_self+m_chassis*d_chassis^2; %Moment of inertial about the center of gravity. 
@@ -20,7 +21,8 @@ d_motor=FSAE_Race_Car.chassis.motor_Z/12; %Distance to be used in parallel axis 
 i_motor_self=.4*m_motor*r_motor^2; %Moment of inertia about itsself.
 i_motor_cg=i_motor_self+m_motor*d_motor^2; %Moment of inertia about cg. 
 %% Driver 
-%leg_length=FSAE_Race_Car.pilot.height*.6/12; %the length of the drivers legs in ft. 
+%leg_length=FSAE_Race_Car.pilot.height*.6/12; %the length of the drivers
+%legs in ft. (Not needed)
 body_length=FSAE_Race_Car.pilot.height*.4/12; %length of the rest of the drivers body in ft. 
 leg_weight=FSAE_Race_Car.pilot.weight*.4/32.174; %weight of the drivers legs in slugs.
 body_weight=FSAE_Race_Car.pilot.weight*.6/32.174; %weight of the rest of the drivers body in slugs.
