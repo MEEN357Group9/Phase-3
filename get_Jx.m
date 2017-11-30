0df069a1e3658769df9d4e7bcc6fe0da439a405d
HAshby <harleyashby@comcast.net>
2017-11-30 08:29:46 -0600

    task 1 complete

--- get_Jx.m
+++ get_Jx.m
@@ -5,14 +5,13 @@ function [Jx] = get_Jx(FSAE_Race_Car)
        %Import:
             %FSAE_Race_Car is a data structor.
         %Output:
-            %Jx is the moment of inertia about the Y axis. 
-            
+            %Jx is the moment of inertia about the Y axis.         
 %% Chassis
 r_chassie=FSAE_Race_Car.chassis.diameter/24; %Finding the radius of the chassis in feet.
 m_chassis=FSAE_Race_Car.chassis.weight/32.174; %Mass of the chassis in slugs.
-l_chassis=FSAE_Race_Car.chassis.length/12; %Length of the chassis in ft.
+%l_chassis=FSAE_Race_Car.chassis.length/12; %Length of the chassis in ft.
 d_chassis=FSAE_Race_Car.chassis.cg_Z/12; %Distance to use in the parallel axis theorm in ft.
-i_chassis_self=1/12*m_chassis*(6*r_chassie^2+l_chassis^2); %Moment of inertia about its own center of gravity.
+i_chassis_self=m_chassis*r_chassie^2; %Moment of inertia about its own center of gravity.
 i_chassis_cg=i_chassis_self+m_chassis*d_chassis^2; %Moment of inertial about the center of gravity. 
 %% Engine 
 r_motor=FSAE_Race_Car.power_plant.diameter/24; %radius of the motor in ft.
@@ -21,13 +20,13 @@ d_motor=FSAE_Race_Car.chassis.motor_Z/12; %Distance to be used in parallel axis 
 i_motor_self=.4*m_motor*r_motor^2; %Moment of inertia about itsself.
 i_motor_cg=i_motor_self+m_motor*d_motor^2; %Moment of inertia about cg. 
 %% Driver 
-leg_length=FSAE_Race_Car.pilot.height*.6/12; %the length of the drivers legs in ft. 
+%leg_length=FSAE_Race_Car.pilot.height*.6/12; %the length of the drivers legs in ft. 
 body_length=FSAE_Race_Car.pilot.height*.4/12; %length of the rest of the drivers body in ft. 
 leg_weight=FSAE_Race_Car.pilot.weight*.4/32.174; %weight of the drivers legs in slugs.
 body_weight=FSAE_Race_Car.pilot.weight*.6/32.174; %weight of the rest of the drivers body in slugs.
 r=FSAE_Race_Car.pilot.girth/(2*pi)/12; %determinig the radius in ft.
 i_leg_self=1/2*leg_weight*r^2; %moment of inertia about itself.
-i_body_self=1/2*body_weight*r^2; %Moment of inertia about its center line.
+i_body_self=1/12*body_weight*(3*r^2+body_length^2); %Moment of inertia about its center line.
 d_leg=(FSAE_Race_Car.chassis.seat_Z+FSAE_Race_Car.pilot.girth/(2*pi))/12; %distance for legs.
 d_body=(FSAE_Race_Car.chassis.seat_Z+FSAE_Race_Car.pilot.girth/(pi)+FSAE_Race_Car.pilot.height/5)/12; %distance for body.
 i_leg_cg=i_leg_self+leg_weight*d_leg^2; %Intertia about cg.
