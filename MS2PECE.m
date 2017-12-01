9157c47125e9da10f8b3cb4a0e188d57846c5304
soniamelsanchez <32579896+soniamelsanchez@users.noreply.github.com>
2017-12-01 11:36:43 -0600

    fuckkk yeah

--- MS2PECE.m
+++ MS2PECE.m
@@ -28,15 +28,15 @@ sX = size(X0);
 sV = size(V0);
 sA = size(A0);
 
-% if sM(1) ~= sM(2) || sC(1) ~= sC(2) || sK(1) ~= sK(2)
-%     error('The M,C, and K matrices must be square in size.');
-% elseif  size(M) ~= size(C) || size(C) ~= size(K) || size(M) ~= size(K)
-%     error('The M,C,and K matrices are not the same size');
-% elseif sX(2) ~= 1 || sV(2) ~= 1 || sA ~= 1
-%     error('X0, V0, and A0 must be column vectors.');
-% elseif sX ~= sV ~= sA
-%     error('X0, V0, and A0 must be column vectors with the same amount of rows.');
-% end
+if sM(1) ~= sM(2) || sC(1) ~= sC(2) || sK(1) ~= sK(2)
+    error('The M,C, and K matrices must be square in size.');
+elseif  sM(1) ~= sC(1) || sC(1) ~= sK(1) || sM(1) ~= sK(1)
+    error('The M,C,and K matrices are not the same size');
+elseif sX(2) ~= 1 || sV(2) ~= 1 || sA(2) ~= 1
+    error('X0, V0, and A0 must be column vectors.');
+elseif sX(1) ~= sV(1) || sV(1) ~= sA(1) || sX(1) ~= sA(1)
+    error('X0, V0, and A0 must be column vectors with the same amount of rows.');
+end
 [siz, ~] = size(X0);
 T = zeros(1);
 X = zeros(1,siz);
@@ -47,6 +47,8 @@ V(1,:) = V0;
 A(1,:) = A0;
 T(1) = D.t_in;
 
+D.t_prev = 0;
+D.X_prev = 0;
 t = D.t_prev;
 
 %% Predict
@@ -62,6 +64,7 @@ V1p = V0 +h.*A0;
 %% Evaluate
 A1p = M^(-1) * ( FN(t,D) - C*V1p - K*X1p);
 
+[~ , D] = get_forcing_function(t,D);
 %% Correct 
 
 t1 = D.t_in + h;
@@ -73,11 +76,12 @@ V1 = V0 + h/2.*(A1p+A0);
 
 A1 = M^(-1) * (FN(t,D) - C*V1 - K*X1);
 
-T(2,:) = t1;
+T(2) = t1;
 X(2,:) = X1;
 V(2,:) = V1;
 A(2,:) = A1;
 
+[~ , D] = get_forcing_function(T(2),D);
 %% Remaining
 for i = 2:D.N
     
@@ -98,7 +102,7 @@ for i = 2:D.N
     X(i+1,:) = X2;
     V(i+1,:) = V2;
     A(i+1,:) = A2;
-    
+    [~ , D] = get_forcing_function(T(i+1),D);
 
 end
 end
